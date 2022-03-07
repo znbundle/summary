c0e6bd76081a618808cd2ea7119543cb3ee9a637
Vitaliy <theyamshikov@yandex.ru>
2022-03-07 19:49:01 +0600

    upd

--- src/Domain/Migrations/m_2021_05_06_052004_create_attempt_table.php
+++ src/Domain/Migrations/m_2021_05_06_052004_create_attempt_table.php
@@ -3,8 +3,8 @@
 namespace Migrations;
 
 use Illuminate\Database\Schema\Blueprint;
-use ZnLib\Migration\Domain\Base\BaseCreateTableMigration;
-use ZnLib\Migration\Domain\Enums\ForeignActionEnum;
+use ZnDatabase\Migration\Domain\Base\BaseCreateTableMigration;
+use ZnDatabase\Migration\Domain\Enums\ForeignActionEnum;
 
 class m_2021_05_06_052004_create_attempt_table extends BaseCreateTableMigration
 {
