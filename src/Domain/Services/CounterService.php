897b057ef9f8e54d523d63fe268419cdaf7ce89d
Vitaliy <theyamshikov@yandex.ru>
2022-05-02 21:01:40 +0600

    upd

--- src/Domain/Services/CounterService.php
+++ src/Domain/Services/CounterService.php
@@ -11,7 +11,7 @@ use ZnCore\Base\Exceptions\AlreadyExistsException;
 use ZnCore\Base\Exceptions\NotFoundException;
 use ZnCore\Domain\Base\BaseCrudService;
 use ZnCore\Domain\Entities\Query\Where;
-use ZnCore\Domain\Interfaces\Entity\EntityIdInterface;
+use ZnCore\Contract\Domain\Interfaces\Entities\EntityIdInterface;
 use ZnCore\Domain\Interfaces\Libs\EntityManagerInterface;
 use ZnCore\Domain\Libs\Query;
 
